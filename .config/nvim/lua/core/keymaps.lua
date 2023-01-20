8299f09b3c459b335727dfa32f5587fe1cba0970
ling-pc <2505660478@qq.com>
2023-01-20 16:03:49 +0800

    update

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+
+keymap.set("i", "jk", "<ESC>")
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gb=gb")
+
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+
--- nvim/lua/plugin-config/bufferline.lua
+++
@@ -1,9 +0,0 @@
-local status, bufferline = pcall(require, "bufferline")
-if not status then
-    vim.notify("没有 bufferline")
-    return
-  end
-
--- bufferline
--- https://github.com/akinsho/bufferline.nvim#configuration
-bufferline.setup({})
