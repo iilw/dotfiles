0f7836f5661a412ea47fb97160e1677ef83db637
ling-pc <2505660478@qq.com>
2023-01-23 16:55:06 +0800

    feat: fish

---
+++ .bash_profile
@@ -0,0 +1,5 @@
+
+# source
+source ~/.aliases;
+source ~/.exports;
+
---
+++ .config/fish/config.fish
@@ -0,0 +1,26 @@
+
+
+set -gx TERM xterm-256color
+
+# theme
+
+
+# aliases
+alias g git
+
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+
+
+# NVM
+function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+  status --is-command-substitution; and return
+
+  if test -f .nvmrc; and test -r .nvmrc;
+    nvm use
+  else
+  end
+end
