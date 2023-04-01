22f974b436ef1355f2e0ae41a014cc383ec065b9
Ling Wang <2505660478@qq.com>
2023-04-01 21:10:28 +0800

    feat: support treesitter vim

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -1,7 +1,12 @@
 # apps... but better
 alias vim=nvim
 
+# proxy
+alias proxy='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
+alias unproxy='export https_proxy= http_proxy= all_proxy='
+
 # git
+alias g=git
 
 
 # ls
