64de549caa7cca1a2427bed33c8ce6818ca8d947
Ling Wang <2505660478@qq.com>
2023-11-04 16:20:32 +0800

    update

--- config/nvim/after/plugin/illuminate.rc.lua
+++ config/nvim/after/plugin/illuminate.rc.lua
@@ -4,6 +4,7 @@ if(not status) then return end
 illuminate.configure({
   delay = 200,
   large_file_cutoff = 2000,
+  under_cursor = false,
   providers = {
     'lsp'
   }
--- config/nvim/plugin/packer_compiled.lua
+++ config/nvim/plugin/packer_compiled.lua
@@ -110,7 +110,7 @@ _G.packer_plugins = {
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["im-select.nvim"] = {
-    config = { "\27LJ\2\ni\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\20default_command\14im-select\20async_switch_im\1\nsetup\14im_select\frequire\0" },
+    config = { "\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14im_select\frequire\0" },
     loaded = true,
     path = "/Users/devling/.local/share/nvim/site/pack/packer/start/im-select.nvim",
     url = "https://github.com/keaising/im-select.nvim"
@@ -255,7 +255,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: im-select.nvim
 time([[Config for im-select.nvim]], true)
-try_loadstring("\27LJ\2\ni\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\20default_command\14im-select\20async_switch_im\1\nsetup\14im_select\frequire\0", "config", "im-select.nvim")
+try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14im_select\frequire\0", "config", "im-select.nvim")
 time([[Config for im-select.nvim]], false)
 
 _G._packer.inside_compile = false
