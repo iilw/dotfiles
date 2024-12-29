7594a1efcface986d4766babfeabaa5d69a87e9f
devasling <devasling@gmail.com>
2024-12-29 16:14:58 +0800

    Upgrade lazynvim to v14

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,55 +1,11 @@
 return {
-  -- Alpha
-  {
-    "nvimdev/dashboard-nvim",
-    enabled = false,
-  },
-
-  -- Noice
-  {
-    "folke/noice.nvim",
-    enabled = false,
-    opts = function(_, opts)
-      table.insert(opts.routes, {
-        filter = {
-          event = "notify",
-          find = "No information available",
-        },
-        opts = { skip = true },
-      })
-
-      opts.presets.lsp_doc_border = true
-    end,
-  },
-
-  -- filename
-  {
-    "b0o/incline.nvim",
-    dependencies = { "craftzdog/solarized-osaka.nvim" },
-    event = "BufReadPre",
-    priority = 1200,
-  },
-
-  -- bufferline
+  { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
   {
     "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    keys = {
-      {
-        "<Tab>",
-        "<Cmd>BufferLineCycleNext<CR>",
-        desc = "Next tab",
-      },
-      {
-        "<S-Tab>",
-        "<Cmd>BufferLineCyclePrev<CR>",
-        desc = "Prev tab",
-      },
-    },
     opts = {
       options = {
         mode = "tabs",
-        show_buffer_close_icon = false,
+        show_buffer_close_icons = false,
         show_close_icon = false,
       },
     },
